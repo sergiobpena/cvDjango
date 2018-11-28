ddc1e06b9886b510b2cb71e144f6d69c8a105b7a
sergiobpena <serxio.botana@gmail.com>
2018-11-28 12:46:41 +0100

    Testeado modelo en admin

--- cv/admin.py
+++ cv/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Niveis , CompInfor , CompProg , FormOficial , Curso , Direccion , DatosPersoais , Sector , ExperienciaProfesional , OutroDato , Curriculum 
+
+admin.site.register(Niveis)
+admin.site.register(CompInfor)
+admin.site.register(CompProg)
+admin.site.register(FormOficial)
+admin.site.register(Curso)
+admin.site.register(Direccion)
+admin.site.register(DatosPersoais)
+admin.site.register(Sector)
+admin.site.register(ExperienciaProfesional)
+admin.site.register(OutroDato)
+admin.site.register(Curriculum)
--- cv/models.py
+++ cv/models.py
@@ -2,75 +2,87 @@ from django.db import models
 
 # Create your models here.
 class Niveis(models.Model):
-    nivel=models.CharField(max_length=50, null=False,blank=False)
+    nivel=models.CharField(max_length=50, null=False,blank=False , verbose_name='Nivel')
     def __str__(self):
         return self.nivel
+    class Meta:
+        verbose_name='Niveis'
     
 class CompInfor(models.Model):
-    tipoCompe=models.CharField(max_length=50)
-    software=models.CharField(max_length=50)
+    tipoCompe=models.CharField(max_length=50,verbose_name='Competencia')
+    software=models.CharField(max_length=50,verbose_name='Software')
     #Ollo comillas principiante
     nivel=models.ForeignKey(Niveis,on_delete=models.SET_NULL,blank=True,null=True)
     def __str__(self):
         return self.software
+    class Meta:
+        verbose_name='Competencias informáticas'
 
-class Comp_Prog(models.Model):
+class CompProg(models.Model):
 
     """
     Modelo que representas as competencias e nivel dos linguaxes de programación
     """
     linguaxe=models.CharField(max_length=50)
     nivel=models.ForeignKey(Niveis,on_delete=models.SET_NULL,blank=True,null=True)
-    outra_info=models.TextField()
+    outra_info=models.TextField(blank=True, verbose_name='Informacion adicional')
 
     def __str__(self):
         return self.linguaxe
+    class Meta:
+        verbose_name='Linguaxes de programación'
 
 class FormOficial (models.Model):
     titulacion=models.CharField(max_length=80)
-    dataInicio=models.DateField()
-    dataFin=models.DateField(null=True,blank=True)
-    escola=models.CharField(max_length=80)
+    dataInicio=models.DateField(verbose_name='Data de inicio')
+    dataFin=models.DateField(null=True,blank=True, verbose_name='Data finalización',help_text="En blanco se estás estudando na actualidade")
+    escola=models.CharField(max_length=80, verbose_name='Centro de estudo')
     informacionAdicional=models.TextField()
 
     class Meta:
-        ordering = ["dataFin"]
+        ordering = ["dataFin","dataInicio"]
+        verbose_name='Formación oficial'
     def __str__(self):
         return self.titulacion
     
-
 class Curso(models.Model):
-    nomeCurso:models.CharField(max_length=80)
-    dataInicio=models.DateField()
-    dataFin=models.DateField(null=True,blank=True)
+    nomeCurso=models.CharField(max_length=80, verbose_name='Nome do curso',blank=True, null=True )
+    escola=models.CharField(max_length=80, verbose_name='Centro de estudo', blank=True, null=True)
+    dataInicio=models.DateField(verbose_name='Data inicio' )
+    dataFin=models.DateField(null=True,blank=True, verbose_name='Data finalización',help_text="En blanco se estás estudando na actualidade")
     duracion=models.CharField(max_length=6)
-    compAdq=models.TextField()
+    compAdq=models.TextField(verbose_name='Competencias adquiridas')
     class Meta:
-        ordering = ["dataFin"]
+        ordering = ["dataFin","dataInicio"]
+        verbose_name='Formación non oficial'
     def __str__(self):
         return self.nomeCurso
 
 class Direccion(models.Model):
-    rua=models.CharField(max_length=80)
-    numPis=models.IntegerField()
-    andar=models.IntegerField()
-    letraPis=models.CharField(max_length=1)
+    rua=models.CharField(max_length=80, verbose_name='Rúa')
+    numPis=models.IntegerField(verbose_name='Portal')
+    andar=models.IntegerField(verbose_name='Piso')
+    letraPis=models.CharField(max_length=1,verbose_name='Letra Piso')
     concello=models.CharField(max_length=40)
     provincia=models.CharField(max_length=40)
 
     def __str__(self):
-        return self.rua + ' ' + self.numPis + ' ' + self.andar + ' ' +  self.concello + ' ' + self.provincia
+        #return self.rua + ' %i ' +  self.concello + ' ' + self.provincia %self.numPis
+        return '%s %i %i %s' % (self.rua, self.numPis,self.andar,self.concello)
 
 class DatosPersoais(models.Model):
 
     nome=models.CharField(max_length=50)
-    apel1=models.CharField(max_length=50)
-    apel2=models.CharField(max_length=50)
-    dataNac=models.DateField()
-    correoe=models.EmailField()
+    apel1=models.CharField(max_length=50 , verbose_name='1º Apelido')
+    apel2=models.CharField(max_length=50, verbose_name='2º Apelido')
+    dataNac=models.DateField(verbose_name='Data de nacemento')
+    correoe=models.EmailField(verbose_name='Correo eléctronico')
+    telefono=models.IntegerField(verbose_name='Numero de telefono',blank=True, null=True)
 
     def __str__(self):
         return self.nome + ' ' + self.apel1 + ' ' + self.apel2 
+    class Meta:
+        verbose_name='Datos persoais'
 
 class Sector(models.Model):
     sector=models.CharField(max_length=50,blank=False,null=False)
@@ -78,19 +90,19 @@ class Sector(models.Model):
         return self.sector
 
 class ExperienciaProfesional(models.Model):
-    posto=models.TextField()
-    empresa=models.TextField()
+    posto=models.CharField(max_length=80)
+    empresa=models.CharField(max_length=80)
     provincia=models.CharField(max_length=50)
     concello=models.CharField(max_length=50)
     funcions=models.TextField()
-    dataInicio=models.DateField()
-    dataFin=models.DateField(null=True,blank=True)
-    sector=models.CharField(max_length=50)
+    dataInicio=models.DateField(verbose_name='Data inicio')
+    dataFin=models.DateField(null=True,blank=True, verbose_name='Data Finalización',help_text="En blanco se continúas na actualidade")
     sector = models.ForeignKey(Sector,on_delete=models.SET_NULL, null=True)    
-    def __str__(slelf):
+    def __str__(self):
         return self.posto + ' ' + self.empresa
     class Meta:
-        ordering = ["dataFin"]
+        ordering = ["dataFin","dataInicio"]
+        verbose_name='Experiencia Profesional'
 
     #def get_absolute_url(self):
     
@@ -102,10 +114,12 @@ class ExperienciaProfesional(models.Model):
         #return reverse('posto', args=[str(self.id)])
 
 class OutroDato(models.Model):
-    texto1=models.CharField(max_length=50)
-    texto2=models.TextField()
+    texto1=models.CharField(max_length=50 , verbose_name='Dato de interés')
+    texto2=models.TextField(verbose_name='Información adicional')
     def __str__(self):
         return self.texto1
+    class Meta:
+        verbose_name='Outros datos de interese'
 
 
 class Curriculum(models.Model):
@@ -115,5 +129,5 @@ class Curriculum(models.Model):
     formOficial=models.ManyToManyField(FormOficial,null=True,blank=True)
     cursos=models.ManyToManyField(Curso,null=True,blank=True)
     compInfor=models.ManyToManyField(CompInfor,null=True,blank=True)
-    compProg=models.ManyToManyField(Comp_Prog,null=True,blank=True)
+    compProg=models.ManyToManyField(CompProg,null=True,blank=True)
     OutroDato=models.ManyToManyField(OutroDato,null=True,blank=True)
